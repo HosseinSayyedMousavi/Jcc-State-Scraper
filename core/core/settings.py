7485dbc678b578fee95fa62dac754b62dc9e8344
hossein.sayyedmousavi <hossein.sayyedmousavi@gmail.com>
2023-12-07 13:41:14 +0330

    config database and .env file

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-!6vd&8@($((q)0e=x*^ei-4wt8n1l%8+k_n6r32k!0-&=0dc&4"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG",cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -121,3 +121,14 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("SCRAPER_DB"),
+        'USER': config("SCRAPER_USER"),
+        'PASSWORD': config("SCRAPER_USER_PASSWORD"),
+        'HOST': config("DB_CONTAINER_NAME"),
+        'PORT': config("DB_INNER_PORT"),
+    }
+}
