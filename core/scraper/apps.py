54f0eb0a9b52586c2415fd6d7235fff4f6e3b50f
hossein.sayyedmousavi@gmail.com <hossein.sayyedmousavi@gmail.com>
2023-12-11 16:04:11 +0330

    debug

--- core/scraper/apps.py
+++ core/scraper/apps.py
@@ -6,6 +6,4 @@ import threading
 class ScraperConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'scraper'
-    def ready(self):
-        from .functions import start_scraper
-        threading.Thread(target=start_scraper).start()
+  
